1a39feab9d93d63a5d0d5bbd703c16c5c2de067c
gratestas <stasgrate@gmail.com>
2022-06-14 00:02:44 +0300

    updated user model

---
+++ backend/models/user.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email address"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [6, "Minimum password length must be at least 6 characters"],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    isConfirmed: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    /* registeredAt: {
+    type: Date,
+    default: Date.now,
+  }, */
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(user.password, salt);
+  user.password = hash;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
