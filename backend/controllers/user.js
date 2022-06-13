1bf6854345e7ea65bec21c7ebbc6e4f34a2ebab2
gratestas <stasgrate@gmail.com>
2022-06-14 00:52:20 +0300

    implemented signup functionality

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,30 @@
+import User from "../models/user.js";
+
 // @desc   Register a new user
 // @route  POST /user/signup
 // @access Public
 export const signup = async (req, res) => {
-  res.json({ message: "Register user" });
+  const { email, password, firstName, lastName } = req.body;
+  try {
+    if (!firstName || !lastName || !email || !password)
+      return res.status(400).json({ message: "Please fill in all fields" });
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    const newUser = await User.create({
+      name: `${firstName} ${lastName}`,
+      email,
+      password: password,
+    });
+    res.status(201).json({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Invalid user data" });
+  }
 };
 
 // @desc   Authenticate a user
