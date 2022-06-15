32ec0421685a6e67aa761d997bd78620c05ba3d2
gratestas <stasgrate@gmail.com>
2022-06-15 23:24:42 +0300

    added react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  return <div className="App mt-20 uppercase">zerno</div>;
-}
+import { Dashboard, Auth, AdminDashboard, Home } from "./pages";
+import { AuthProvider } from "./context/AuthContext";
+
+const App = () => {
+  return (
+    <Router>
+      <AuthProvider>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/account" element={<Dashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} exact />
+            <Route path="/auth" element={<Auth />} exact />
+          </Routes>
+        </main>
+      </AuthProvider>
+    </Router>
+  );
+};
 
 export default App;
