16705278eb00910128ea18e5c52c8b140482de9f
gratestas <stasgrate@gmail.com>
2022-06-15 23:24:02 +0300

    created mockup components

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './Dashboard';
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as Auth } from './Auth';
+export { default as Home } from './Home';
