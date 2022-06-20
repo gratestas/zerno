dbdf59d6a1f4d7ee05cff9fdc893320a3f3c3727
gratestas <stasgrate@gmail.com>
2022-06-20 23:17:45 +0300

    added protected admin route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Dashboard, Auth, AdminDashboard, Home } from "./pages";
+import { Dashboard, Auth, AdminDashboard, Home, Unauthorized } from "./pages";
 import { AuthProvider } from "./context/AuthContext";
 import RequireAuth from "./routes/RequireAuth";
+import RequireAdmin from "./routes/RequireAdmin";
 
 const App = () => {
   return (
@@ -12,8 +13,13 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/auth" element={<Auth />} />
+            <Route path="/unauthorized" element={<Unauthorized />} />
+
             <Route element={<RequireAuth />}>
               <Route path="/account" element={<Dashboard />} />
+            </Route>
+
+            <Route element={<RequireAdmin />}>
               <Route path="/admin" element={<AdminDashboard />} />
             </Route>
           </Routes>
