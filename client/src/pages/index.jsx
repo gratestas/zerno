690724c6120bca1c50d1b9f7efdad8d4ae9926c7
gratestas <stasgrate@gmail.com>
2022-06-20 23:21:58 +0300

    added Unauthorized page to export default list

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,4 +1,5 @@
-export { default as Dashboard } from './Dashboard';
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as Auth } from './Auth';
-export { default as Home } from './Home';
+export { default as Dashboard } from "./Dashboard";
+export { default as AdminDashboard } from "./AdminDashboard";
+export { default as Auth } from "./Auth";
+export { default as Home } from "./Home";
+export { default as Unauthorized } from "./Unauthorized";
