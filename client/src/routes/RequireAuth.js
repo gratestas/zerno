4fcaac0c2dfcacd7f4a82bacb8b9d9013382d9ac
gratestas <stasgrate@gmail.com>
2022-06-20 23:14:59 +0300

    created protected admin route

--- client/src/routes/RequireAuth.js
+++ client/src/routes/RequireAuth.js
@@ -2,7 +2,7 @@ import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
 const RequireAuth = () => {
-  const { user, token } = useAuth();
+  const { token } = useAuth();
   const location = useLocation();
   console.log("requreAut", token);
   return token ? (
