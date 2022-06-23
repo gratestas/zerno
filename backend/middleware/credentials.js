60c02d20d9861f4d878f8443fea1e486ab060901
gratestas <stasgrate@gmail.com>
2022-06-23 22:46:41 +0300

    added additional headers

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -3,7 +3,12 @@ import allowedOrigins from "../config/allowedOrigins.js";
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
+    res.header("Content-Type", "application/json;charset=UTF-8");
     res.header("Access-Control-Allow-Credentials", true);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
   }
   next();
 };
