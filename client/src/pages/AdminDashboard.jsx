8aba3c10dda1dd82899d0bc6d907ff89deaefb13
gratestas <stasgrate@gmail.com>
2022-06-24 15:59:41 +0300

    adding protected API endpoint to access users data

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
+  const refresh = useRefreshToken();
   return (
     <div>
       <div>Admin Page</div>
-      <Link to='/account' >To Dashboard</Link>
+      <Link to="/account">To Dashboard</Link>
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
