75f295d04aac3150a58086f7688ea38059e48ca0
gratestas <stasgrate@gmail.com>
2022-06-29 17:21:16 +0300

    changed name of dynamic parameter

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,6 +6,6 @@ import { signin, signup, signout, verify } from "../controllers/auth.js";
 router.post("/signin", signin);
 router.post("/signup", signup);
 router.post("/signout", signout);
-router.get("/verify/:link", verify);
+router.get("/verify/:token", verify);
 
 export default router;
