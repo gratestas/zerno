a189355e13f68a6e9f107632db1e093ecdebfbc8
gratestas <stasgrate@gmail.com>
2022-07-01 22:37:29 +0300

    added middleware to route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,12 +7,19 @@ import {
   signout,
   verify,
   forgotPassword,
+  resetPassword,
 } from "../controllers/auth.js";
+import { validateResetPasswordToken } from "../middleware/auth.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
 router.post("/signout", signout);
 router.get("/verify/:token", verify);
 router.post("/forgot-password", forgotPassword);
+router.post(
+  "/reset-password/:id/:token",
+  validateResetPasswordToken,
+  resetPassword
+);
 
 export default router;
