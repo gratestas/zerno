ffb41f40bbec815fdd722f2dc6319635c10afba4
gratestas <stasgrate@gmail.com>
2022-07-02 00:40:20 +0300

    added route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Dashboard, Auth, AdminDashboard, Home, Unauthorized } from "./pages";
+import {
+  Dashboard,
+  Auth,
+  AdminDashboard,
+  Home,
+  ForgotPassword,
+  Unauthorized,
+} from "./pages";
 import { AuthProvider } from "./context/AuthContext";
 import RequireAuth from "./routes/RequireAuth";
 import PersistentAuth from "./routes/PersistentAuth";
@@ -9,10 +16,11 @@ const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <main className="flex justify-center items-center h-screen">
+        <main className="flex flex-col justify-center items-center h-screen">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/auth" element={<Auth />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/unauthorized" element={<Unauthorized />} />
 
             {/* PRIVATE ROUTES */}
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,4 +2,5 @@ export { default as Dashboard } from "./Dashboard";
 export { default as AdminDashboard } from "./AdminDashboard";
 export { default as Auth } from "./Auth";
 export { default as Home } from "./Home";
+export { default as ForgotPassword } from "./ForgotPassword";
 export { default as Unauthorized } from "./Unauthorized";
