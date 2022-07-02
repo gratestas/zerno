9453c3b657eb54a00de5ea08766bd423cb91d0ff
gratestas <stasgrate@gmail.com>
2022-07-02 17:15:22 +0300

    implemented password reset page

---
+++ client/src/pages/ResetPassword.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { resetPassword } from "../api/auth";
+
+import Button from "../components/button";
+import Input from "../components/input";
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState();
+  const { id, token } = useParams();
+  const [sent, setSent] = useState(false);
+  const handleChange = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const handleClick = async () => {
+    await resetPassword(password, id, token);
+    setSent(true);
+  };
+  return (
+    <>
+      <div className=" flex flex-col justify-center mt-20 mx-20">
+        {!sent ? (
+          <>
+            <h1 className="mb-4 font-bold">Reset passowrd?</h1>
+            <p>Please enter a new passowrd</p>
+            <Input
+              name="New password"
+              label="New Password"
+              onChange={handleChange}
+            />
+            <div onClick={handleClick}>
+              <Button
+                type="submit"
+                color="white"
+                bgColor="black"
+                borderRadius="10px"
+                size="md"
+                text="Confirm"
+              />
+            </div>
+          </>
+        ) : (
+          <h1>Your password was updated successfully!</h1>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ResetPassword;
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,4 +3,5 @@ export { default as AdminDashboard } from "./AdminDashboard";
 export { default as Auth } from "./Auth";
 export { default as Home } from "./Home";
 export { default as ForgotPassword } from "./ForgotPassword";
+export { default as ResetPassword } from "./ResetPassword";
 export { default as Unauthorized } from "./Unauthorized";
