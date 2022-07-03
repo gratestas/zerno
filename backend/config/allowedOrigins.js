aa8cc5155602188f213be71629bfd27db39b8486
gratestas <stasgrate@gmail.com>
2022-07-03 13:33:54 +0300

    added heroku origin

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://www.zernofirin.com",
   "https://zernobakery.netlify.app",
+  "https://zerno-bakery-app.herokuapp.com",
   "http://localhost:5000",
   "http://localhost:3000",
 ];
